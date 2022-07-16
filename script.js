d30ff716624677f685bfe9bad29fc1284582301a
marina larosa <97920018+sara-bahri@users.noreply.github.com>
2022-07-17 00:22:16 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelector(".menu").
+addEventListener('click', () => {
+    document.querySelector(".container")
+    .classList.toggle("change");
+})
+
+document.querySelector(".scroll-btn")
+addEventListener('click', () => {
+
+    document.querySelector("html").style
+    .scrollBehavior = "smooth";
+    setTimeout(() => {
+        document.querySelector("html").style
+        .scrollBehavior = "unset";
+    }, 1000 )
+    
+}
+)
